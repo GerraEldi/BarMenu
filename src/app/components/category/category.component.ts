dcf4dbbf9d956474d93f41d8a0169cce14c55038
Eldi Gerra <eldi.gerra@chweb.it>
2022-10-23 01:25:22 +0200

    added bill popup and product dynamic color generation

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Category, Product } from 'src/app/models/bar.model';
 import { ProductsService } from 'src/services/products.service';
-
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
@@ -10,9 +10,12 @@ import { ProductsService } from 'src/services/products.service';
 })
 export class CategoryComponent implements OnInit {
   productsList!: Product[];
+  @ViewChild('templateBottomSheet')
+  TemplateBottomSheet!: TemplateRef<any>;
   constructor(
     private route: ActivatedRoute,
-    private productsService: ProductsService
+    private productsService: ProductsService,
+    private bottomSheet: MatBottomSheet
   ) { }
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -20,7 +23,6 @@ export class CategoryComponent implements OnInit {
       console.log(categoryName);
       this.filterProductsByCategory(categoryName);
     })
-
   }
 
   filterProductsByCategory(categoryName: string | null): void {
@@ -34,6 +36,22 @@ export class CategoryComponent implements OnInit {
         console.log(err);
       }
     })
+  }
+   openTemplateSheetMenu() {
+    this.bottomSheet.open(this.TemplateBottomSheet);
+  }
+
+  closeTemplateSheetMenu() {
+    this.bottomSheet.dismiss();
+  }
+   getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    console.log(color);
+    return color;
   }
 
 }
--- src/app/components/category/category.module.ts
+++ src/app/components/category/category.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { CategoryRoutingModule } from './category-routing.module';
 import { CategoryComponent } from './category.component';
-
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { BillDetailsPopupModule } from 'src/shared/web-components/bill-details-popup/bill-details-popup.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  declarations: [CategoryComponent],
+  declarations: [
+    CategoryComponent
+  ],
   imports: [
     CommonModule,
-    CategoryRoutingModule
+    CategoryRoutingModule,
+    MatBottomSheetModule,
+    BillDetailsPopupModule,
+    FlexLayoutModule
   ]
 })
 export class CategoryModule { }
